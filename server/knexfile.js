79c194a8b679598aae1b11fb6f59bb52480a7f53
RahmattAmin <rahmatamin0@gmail.com>
2023-10-07 21:28:38 +0700

    feat: add new branch

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,7 +6,7 @@
 export default {
   development: {
     client: "postgresql",
-    connection: "postgresql://postgres:ace16@localhost:5432/recipe_platinum",
+    connection: "postgresql://root:root@localhost:5434/recipe-book",
   },
   migrations: {
     directory: "./src/db/migrations",
