52b5bab5b7fdf21819195a6692137510ce9cc4aa
RahmattAmin <rahmatamin0@gmail.com>
2023-10-24 23:05:32 +0700

    feat: add middleware

---
+++ server/src/middlewares/middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import responseError from "../helpers/restResponse.helper.js";
+
+const JWT_KEY = process.env.JWT_SECRET;
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  const access_token = token.split(" ")[1];
+
+  if (!access_token) {
+    return res.status(401).json(responseError("Access token is missing"));
+  }
+
+  jwt.verify(access_token, JWT_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json(responseError("Invalid Access Token"));
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+};
+
+export default verifyToken;
