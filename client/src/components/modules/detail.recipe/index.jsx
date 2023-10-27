b454403ea7899c4ad8d0331683f9bbfab230bdda
Dimas Rachmadika <dimasrachmadika@gmail.com>
2023-10-27 13:25:27 +0700

    chore: detail recipe

--- client/src/components/modules/detail.recipe/index.jsx
+++ client/src/components/modules/detail.recipe/index.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailRecipe = () => {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState([]);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/v1/recipebook/recipes/:id`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch recipe");
+      }
+      const data = await response.json();
+      setRecipe(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
   return <div>DetailRecipe</div>;
 };
 
