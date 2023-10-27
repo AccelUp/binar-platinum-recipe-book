92891df06e1d71a9f1daf0916fc339b53a88a67b
Dimas Rachmadika <dimasrachmadika@gmail.com>
2023-10-27 13:27:01 +0700

    chore: detail recipe

--- client/src/components/modules/detail.recipe/index.jsx
+++ client/src/components/modules/detail.recipe/index.jsx
@@ -22,7 +22,30 @@ const DetailRecipe = () => {
   useEffect(() => {
     fetchRecipes();
   }, []);
-  return <div>DetailRecipe</div>;
+  return (
+    <>
+      <section className="text-gray-600 body-font md:mt-24 bg-putih overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h2 className="text-sm title-font uppercase font-bold text-gray-500 tracking-widest">
+                {recipe.category}
+              </h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
+                {recipe.title}
+              </h1>
+
+              <p className="leading-relaxed">{recipe.description}</p>
+            </div>
+            <img
+              className="lg:w-1/2 w-full lg:h-auto max-h-[400px] md:mt-0 mt-7 object-contain object-center rounded"
+              src={recipe.image}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default DetailRecipe;
